795cc43b325adbb6da9bc3e46198a7d0d73f4369
zhengtianbao <china.zhengtianbao@gmail.com>
2024-03-27 10:20:58 +0800

    move prometheus data directory

--- scripts/start-prometheus.sh
+++ scripts/start-prometheus.sh
@@ -4,7 +4,9 @@ source "/vagrant/scripts/common.sh"
 
 function startServices {
   echo "starting prometheus service"
-  nohup /usr/local/prometheus/prometheus --config.file="/usr/local/prometheus/prometheus.yml" > ${PROMETHEUS_LOG_DIR}/prometheus.log 2>&1 &
+  nohup /usr/local/prometheus/prometheus \
+    --storage.tsdb.path=/usr/local/prometheus/data \
+    --config.file="/usr/local/prometheus/prometheus.yml" > ${PROMETHEUS_LOG_DIR}/prometheus.log 2>&1 &
 }
 
 echo "start prometheus"
